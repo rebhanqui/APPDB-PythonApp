711f8406cf22e43b1de8446a6487b29eea85279d
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2024-05-02 11:34:11 +0100

    function one edited and comments added/refs

--- sqlqueries.py
+++ sqlqueries.py
@@ -14,31 +14,52 @@ def connect():
             cursorclass=pymysql.cursors.DictCursor
         )
         return conn
+    #returns error and detail if connection fails
+    #https://stackoverflow.com/questions/41344171/handling-pymysql-exceptions-best-practices
     except pymysql.Error as Error:
         print("Error connecting to the database:", Error)
         return None
 
 # Question 1
 def viewCitiesByCountry():
-    if (not conn):
-        connect();
+    connect()
 
-    sql = """SELECT country.Name AS Country, city.Name AS City Name, city.District AS District Name, city.Population 
-    FROM country 
-    INNER JOIN city 
-    ORDER BY country.Name 
-    WHERE country = %s"""
+    try:
+        with conn.cursor() as cursor:
+            countryName = input("Enter the name of a country: ")
+            query = """SELECT country.Name, city.Name, city.District, city.Population, city.latitude, city.longitude 
+                    FROM country 
+                    JOIN city ON country.Code = city.CountryCode 
+                    WHERE country.Name LIKE %s"""
+            cursor.execute(query, (countryName,))
+            cities = cursor.fetchall()
 
-    with conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (countryname))
-        result = cursor.fetchall()
-        print(result)
+#if cities are found in database under country name then code iterates through results of sql query
+#results then display unless none found and else prints this            
+            if cities:
+                chosenCountry = None
+                for city in cities:
+                    if chosenCountry != city['Name']:
+                        chosenCountry = city['Name']
+                        print(f"Country: {chosenCountry}")
+                    print(f"City: {city['Name']}")
+                    print(f"District: {city['District']}")
+                    print(f"Population: {city['Population']}")
+                    print()
+            else:
+                print(f"No cities found for {countryName}")
+#https://realpython.com/python-f-strings/
+    except pymysql.Error as Error:
+        print("MySQL Error:", Error)
+        return None
+    finally:
+        conn.close
+#https://www.geeksforgeeks.org/try-except-else-and-finally-in-python/
 
 # Question 2           
 def updateCityPopulation():
-    if (not conn):
-            connect();
+    if not conn:
+        connect()
     
     sql = """SELECT city.ID, city.Name, city.CountryCode, city.Population, city.latitude, city.longitude 
     FROM city; 
@@ -55,8 +76,7 @@ def updateCityPopulation():
 
 # Question 3         
 def addPerson():
-    if (not conn):
-            connect();
+    connect()
     
     sql = "INSERT INTO person VALUES (%s, %s, %s, %s, %s);"
     cursor.execute(sql)
@@ -77,8 +97,7 @@ def viewCountriesByPopulation():
 
 # Question 5            
 def getTwinnedCities():
-    if (not conn):
-            connect();
+    connect()
     
     sql = "SELECT country.Code, country.Name, country.Continent, country.Population FROM country ORDER BY country.Code"
     
